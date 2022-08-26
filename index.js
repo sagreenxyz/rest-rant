28273067fadcafcdb4bfc1e7af2cdd4a02d06807
Scott A. Green <sagreenxyz@gmail.com>
2022-08-26 18:35:31 -0500

    adding styles css file

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 app.use('/places', require('./controllers/places'))
 
--- views/default.jsx
+++ views/default.jsx
@@ -6,6 +6,7 @@ function Def(props) {
             <head>
                 <title>Title</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"></link>
+                <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
                 {props.children}
