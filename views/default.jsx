46461c89c1e5e1fbd90c27c24144a6a627ce9f8f
Scott A. Green <sagreenxyz@gmail.com>
2022-08-26 18:29:56 -0500

    adding bootstrapcss reference and button element to test import link for success

--- views/default.jsx
+++ views/default.jsx
@@ -5,6 +5,7 @@ function Def(props) {
         <html>
             <head>
                 <title>Title</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"></link>
             </head>
             <body>
                 {props.children}
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,9 @@ function Home() {
         <Def>
             <main>
                 <h1>Home Page</h1>
+                <a href="/places">
+                    <button className="btn btn-primary">Places Page</button>
+                </a>
             </main>
         </Def>
     )
