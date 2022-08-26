40f7ed4df4b870c569aa01922f5b8b5b491e064b
Scott A. Green <sagreenxyz@gmail.com>
2022-08-26 18:22:32 -0500

    adding mock places data to git handler in places controller

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,19 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+    }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+    }]
     res.status(200).render('places/index')
 })
 
