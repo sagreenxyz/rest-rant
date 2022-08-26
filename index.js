487a0c3b8f4583a829191b80f03676a062ae62fa
Scott A. Green <sagreenxyz@gmail.com>
2022-08-26 17:55:36 -0500

    adding wildcard catch route with 404 response

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!')
+    res.status(200).send('Hello, World!')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page Not Found</h1>')
 })
 
 app.listen(process.env.PORT, () => {
