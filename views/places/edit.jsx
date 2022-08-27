50220109cddfd87f7ed19778749fc9f4c53cfa51
Scott A. Green <sagreenxyz@gmail.com>
2022-08-26 20:51:11 -0500

    improving edit form view appearance

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -7,25 +7,31 @@ function Edit_Form({place, id}) {
             <main>
                 <h1>Edit Place</h1>
                 <form method="POST" action={`/places/${id}?_method=PUT`}>
-                    <div className="form-group">
-                        <label htmlFor="name">Place Name</label>
-                        <input className="form-control" id="name" name="name" defaultValue={place.name} required />
+                    <div className="row">
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="name">Place Name</label>
+                            <input className="form-control" id="name" name="name" defaultValue={place.name} required />
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="pic">Place Picture</label>
+                            <input className="form-control" id="pic" name="pic" defaultValue={place.pic} />
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="pic">Place Picture</label>
-                        <input className="form-control" id="pic" name="pic" defaultValue={place.pic} />
+                    <div className="row">
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="city">City</label>
+                            <input className="form-control" id="city" name="city" defaultValue={place.city} />
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="state">State</label>
+                            <input className="form-control" id="state" name="state" defaultValue={place.state} />
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="city">City</label>
-                        <input className="form-control" id="city" name="city" defaultValue={place.city} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="state">State</label>
-                        <input className="form-control" id="state" name="state" defaultValue={place.state} />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="cuisines">Cuisines</label>
-                        <input className="form-control" id="cuisines" name="cuisines" defaultValue={place.cuisines} required />
+                    <div className="row">
+                        <div className="form-group col-sm-12">
+                            <label htmlFor="cuisines">Cuisines</label>
+                            <input className="form-control" id="cuisines" name="cuisines" defaultValue={place.cuisines} required />
+                        </div>
                     </div>
                     <input className="btn btn-primary" type="submit" value="Save" />
                 </form>
