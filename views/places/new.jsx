38ff748cd1323daaa40ecfa2fe26dc44604e7415
Scott A. Green <sagreenxyz@gmail.com>
2022-08-27 12:38:57 -0500

    adding founded year default value to new place view

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -34,7 +34,7 @@ function New_Form() {
                         </div>
                         <div className="form-group col-sm-6 col-md-4 col-lg-3">
                             <label for="founded">Founded Year</label>
-                            <input className="form-control" id="founded" name="founded" />
+                            <input type="number" className="form-control" id="founded" name="founded" value={new Date().getFullYear()} />
                         </div>
                     </div>
                     <input className="btn btn-primary" type="submit" value="Add Place" />
