79c0bb53e9451bb7ed7a74a1ee7b42ea8974138b
Scott A. Green <sagreenxyz@gmail.com>
2022-08-26 21:00:08 -0500

    added edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -72,6 +72,20 @@ router.put('/:id', (req, res) => {
         res.render('error404')
     }
     else {
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+
+        // Save the new data into places[id]
+        places[id] = req.body
         res.redirect(`/places/${id}`)
     }
 })
