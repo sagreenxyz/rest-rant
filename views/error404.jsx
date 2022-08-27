32c510e5178318a091a29e51a7fafa9d88635073
Scott A. Green <sagreenxyz@gmail.com>
2022-08-26 19:09:25 -0500

    adding dropped-ice-cream-cone.jpg image to error 404 page component

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,9 @@ function Error404() {
         <Def>
             <main>
                 <h1>404: Page Not Found</h1>
+                <div>
+                    <img src="/images/dropped-ice-cream-cone.jpg" alt="Dropped Ice Cream Cone" width="500px" />
+                </div>
                 <p>Oops, sorry...  We can't find this page!</p>
             </main>
         </Def>
