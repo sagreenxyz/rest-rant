f81ab09f2af09df54d1a7c1efe9f9a63c8062171
Scott A. Green <sagreenxyz@gmail.com>
2022-08-26 19:20:04 -0500

    improving positioning of place components

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -4,9 +4,15 @@ const Def = require('../default')
 function Index({ places }) {
     const placesMap = places.map(place => {
         return (
-            <div className="col-sm-6">
-                <h2>{place.name}</h2>
-                <img src={place.pic} alt={place.name} width="300px" />
+            <div className="col-sm-4">
+                <h2 className="text-center">{place.name}</h2>
+                <p className="text-center">
+                    {place.cuisines}
+                </p>
+                <img className="mx-auto d-block" src={place.pic} alt={place.name} width="300px" />
+                <p className="text-center">
+                    Located in {place.city}, {place.state}
+                </p>
             </div>
         )
     })
