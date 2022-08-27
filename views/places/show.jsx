71ea80171ec0b011ae0a99b8b37c0c33f8135081
Scott A. Green <sagreenxyz@gmail.com>
2022-08-26 19:53:39 -0500

    rendering more place data in show view component

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,6 +6,13 @@ function Show({ place }) {
         <Def>
             <main>
                 <h1>{place.name}</h1>
+                <img src={place.pic} alt={place.name} width="300px" />
+                <h3>Rating</h3>
+                <p>Not Rated</p>
+                <h3>Description</h3>
+                <p>Located in {place.city}, {place.state} and serving {place.cuisines}</p>
+                <h3>Comments</h3>
+                <p>No comments yet!</p>
             </main>
         </Def>
     )
