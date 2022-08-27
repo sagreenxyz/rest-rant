34f6bf6033aee31fa0a2f4d716f22379fdf5508c
Scott A. Green <sagreenxyz@gmail.com>
2022-08-27 14:51:39 -0500

    adding seed-comments file and logic

---
+++ seeders/seed-comments.js
@@ -0,0 +1,16 @@
+const db = require('../models')
+
+async function seed() {
+    let place = await db.Place.findOne({ name: 'H-Thai-ML' })
+    let comment = await db.Comment.create({
+        author: 'Famished Fran',
+        rant: false,
+        stars: 5.0,
+        content: 'Wow, simply amazing! Highly recommended!'
+    })
+    place.comments.push(comment._id)
+    await place.save()
+    process.exit()
+}
+
+seed()
