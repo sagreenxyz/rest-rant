b3cc11d208e526cee611c87682e5a1c516199361
Scott A. Green <sagreenxyz@gmail.com>
2022-08-26 19:42:40 -0500

    adding navbar with links

--- views/default.jsx
+++ views/default.jsx
@@ -9,6 +9,13 @@ function Def(props) {
                 <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/places">Places</a></li>
+                        <li><a href="/places/new">Add Place</a></li>
+                    </ul>
+                </nav>
                 {props.children}
             </body>
         </html>
