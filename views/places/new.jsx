88d4db543885ba1332d75899161336cc687136d9
Scott A. Green <sagreenxyz@gmail.com>
2022-08-26 19:29:59 -0500

    improved appearance of new place view form

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -7,25 +7,25 @@ function New_Form() {
             <main>
                 <h1>Add a New Place</h1>
                 <form method="POST" action="/places">
-                    <div>
+                    <div className="form-group">
                         <label htmlFor="name">Place Name</label>
-                        <input id="name" name="name" required />
+                        <input className="form-control" id="name" name="name" required />
                     </div>
-                    <div>
+                    <div className="form-group">
                         <label htmlFor="pic">Place Picture</label>
-                        <input id="pic" name="pic" />
+                        <input className="form-control" id="pic" name="pic" />
                     </div>
-                    <div>
+                    <div className="form-group">
                         <label htmlFor="city">City</label>
-                        <input id="city" name="city" />
+                        <input className="form-control" id="city" name="city" />
                     </div>
-                    <div>
+                    <div className="form-group">
                         <label htmlFor="state">State</label>
-                        <input id="state" name="state" />
+                        <input className="form-control" id="state" name="state" />
                     </div>
-                    <div>
+                    <div className="form-group">
                         <label htmlFor="cuisines">Cuisines</label>
-                        <input id="cuisines" name="cuisines" required />
+                        <input className="form-control" id="cuisines" name="cuisines" required />
                     </div>
                     <input className="btn btn-primary" type="submit" value="Add Place" />
                 </form>
