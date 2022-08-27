d05e9c9ed728bfc801eb6bb75fe3f3cd2c475558
Scott A. Green <sagreenxyz@gmail.com>
2022-08-26 20:49:04 -0500

    edit place form setup with default values

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,14 +1,37 @@
 const React = require('react')
 const Def = require('../default.jsx')
 
-function edit_form() {
+function Edit_Form({place, id}) {
     return (
         <Def>
             <main>
                 <h1>Edit Place</h1>
+                <form method="POST" action={`/places/${id}?_method=PUT`}>
+                    <div className="form-group">
+                        <label htmlFor="name">Place Name</label>
+                        <input className="form-control" id="name" name="name" defaultValue={place.name} required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="pic">Place Picture</label>
+                        <input className="form-control" id="pic" name="pic" defaultValue={place.pic} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="city">City</label>
+                        <input className="form-control" id="city" name="city" defaultValue={place.city} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="state">State</label>
+                        <input className="form-control" id="state" name="state" defaultValue={place.state} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="cuisines">Cuisines</label>
+                        <input className="form-control" id="cuisines" name="cuisines" defaultValue={place.cuisines} required />
+                    </div>
+                    <input className="btn btn-primary" type="submit" value="Save" />
+                </form>
             </main>
         </Def>
     )
 }
 
-module.exports = edit_form
+module.exports = Edit_Form
