b49a26247153b4a24ab32806ec6198490a159f7a
Scott A. Green <sagreenxyz@gmail.com>
2022-08-26 20:52:57 -0500

    improving new form view appearance

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -7,25 +7,31 @@ function New_Form() {
             <main>
                 <h1>Add a New Place</h1>
                 <form method="POST" action="/places">
-                    <div className="form-group">
-                        <label htmlFor="name">Place Name</label>
-                        <input className="form-control" id="name" name="name" required />
+                    <div className="row">
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="name">Place Name</label>
+                            <input className="form-control" id="name" name="name" required />
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="pic">Place Picture</label>
+                            <input className="form-control" id="pic" name="pic" />
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="pic">Place Picture</label>
-                        <input className="form-control" id="pic" name="pic" />
+                    <div className="row">
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="city">City</label>
+                            <input className="form-control" id="city" name="city" />
+                        </div>
+                        <div className="form-group col-sm-6">
+                            <label htmlFor="state">State</label>
+                            <input className="form-control" id="state" name="state" />
+                        </div>
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="city">City</label>
-                        <input className="form-control" id="city" name="city" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="state">State</label>
-                        <input className="form-control" id="state" name="state" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="cuisines">Cuisines</label>
-                        <input className="form-control" id="cuisines" name="cuisines" required />
+                    <div className="row">
+                        <div className="form-group col-sm-12">
+                            <label htmlFor="cuisines">Cuisines</label>
+                            <input className="form-control" id="cuisines" name="cuisines" required />
+                        </div>
                     </div>
                     <input className="btn btn-primary" type="submit" value="Add Place" />
                 </form>
