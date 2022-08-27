99616e8acb320fb8a34fc98f6a6a9e797946505f
Scott A. Green <sagreenxyz@gmail.com>
2022-08-26 20:04:39 -0500

    configuring the method-override middleware

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
@@ -8,6 +9,7 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.use('/places', require('./controllers/places'))
 
